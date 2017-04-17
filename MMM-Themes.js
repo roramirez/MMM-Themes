dccb6a1688fdd49578ef5770b277b9beb2407346
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-04-17 11:36:54 -0300

    Add first theme valentine base in https://github.com/morozgrafix/MMM-Valentine of @morozgrafix

---
+++ MMM-Themes.js
@@ -0,0 +1,76 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-Themes
+ *
+ * By Rodrigo Ramírez Norambuena
+ * MIT Licensed.
+ */
+
+Module.register("MMM-Themes", {
+
+	defaults: {
+		count: 10,
+		size: 1.00,
+		theme: "valentine",
+	},
+
+	requiresVersion: "2.1.0", // Required version of MagicMirror
+
+	// Availables themes
+	themes: {
+		valentine: {
+			wrapper: "valentine-wrapper",
+			items: 14,
+			iconClassName: "valentine-heart",
+			jiggleClassName: "heart"
+		}
+	},
+
+	getStyles: function() {
+		return [this.data.path + "/themes/" + this.getThemeName() + "/style.css"];
+	},
+
+	getThemeName: function() {
+		return this.config.theme.toLowerCase();
+	},
+
+	getTheme: function() {
+		return this.themes[this.getThemeName()];
+	},
+
+	getDom: function() {
+
+		var theme = this.getTheme();
+		var wrapper = document.createElement("div")
+
+		wrapper.className = theme.wrapper;
+
+		var elementIcon, jiggle, size, rotation;
+
+		for(var i = 0; i < this.config.count; i++) {
+			elementIcon = document.createElement("div");
+			elementIcon.className = theme.iconClassName;
+
+			jiggle = document.createElement("div");
+			jiggle.style.animationDelay = (Math.random() * 4) + "s";
+			jiggle.style.animationDuration = ((Math.random() * 30) + 30) + "s";
+			jiggle.className = theme.jiggleClassName + (Math.round(Math.random() * theme.items) + 1);
+
+			size = (Math.random() * 0.75) + 0.25 + this.config.size;
+			rotation = (Math.floor(Math.random() * 81) - 40);
+			jiggle.style.transform = "scale(" + size + ", " + size + ") rotate(" + rotation + "deg)";
+			jiggle.style.opacity = size - this.config.size;
+
+			elementIcon.appendChild(jiggle);
+			elementIcon.style.left = ((Math.random() * 100) - 10) + "%";
+			elementIcon.style.animationDelay = (Math.random() * 100) + "s";
+			elementIcon.style.animationDuration = 100 - (Math.random() * 50 * (size - this.config.size)) + "s";
+
+			wrapper.appendChild(elementIcon);
+
+		}
+		return wrapper;
+	},
+
+});
---
+++ README.md
@@ -0,0 +1,32 @@
+# MMM-Themes
+
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/).
+
+Provider themes for MagicMirror
+
+## Using the module
+
+To use this module, add the following configuration block to the modules array in the `config/config.js` file:
+
+
+```js
+var config = {
+    modules: [
+        {
+            module: "MMM-Themes",
+			position: "fullscreen_above",
+            config: {
+                // See below for configurable options
+            }
+        }
+    ]
+}
+```
+
+## Configuration options
+
+| Option      | Description
+|------------ |-----------
+| `count`     | The number theme icons to fall. More icons are havier for the cpu, so don't go wild. <br><b>Default value:</b> <code>10</code>
+| `size`      | Scale ratio for each icon, to help adjustments for different screen resolutions. <br><b>Default value:</b> <code>1.00</code>
+| `theme`     | The theme name to show. Availables `valentine`<br> <b>Default:</b><code>valentine</code>
